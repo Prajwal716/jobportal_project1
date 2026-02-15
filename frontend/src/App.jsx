ef65f9c3e7555f137459a1609fde7bc7e04a4b41
Prajwal716 <prajwalmujagond@gmail.com>
2026-02-15 12:12:06 +0530

    production ready app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,83 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import Browse from './components/Browse'
+import Profile from './components/Profile'
+import JobDesciption from './components/JobDesciption'
+import Companies from './components/admin/Companies'
+import CompanyCreate from './components/admin/CompanyCreate'
+import CompanySetup from './components/admin/CompanySetup'
+import AdminJobs from './components/admin/AdminJobs'
+import PostJob from './components/admin/PostJob'
+import Applicants from './components/admin/Applicants'
+import ProtectedRoute from './components/admin/ProtectedRoute'
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+  {
+    path: "/jobs",
+    element: <Jobs />
+  },
+  {
+    path: "/description/:id",
+    element: <JobDesciption />
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+  //For admin
+  {
+    path:"/admin/companies",
+    element:<Companies/> //if you want to protect admin page from student then change it <ProtectedRoute><Companies/></ProtectedRoute> and same for others too.
+  },
+  {
+    path:"/admin/companies/create",
+    element:<CompanyCreate/>
+  },
+  {
+    path:"/admin/companies/:id",
+    element:<CompanySetup/>
+  },
+  {
+    path:"/admin/jobs",
+    element:<AdminJobs/>
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<PostJob/>
+  },
+  {
+    path:"/admin/jobs/:id/applicants",
+    element:<Applicants/>
+  },
+
+])
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/redux/applicationSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+import reducer from "./authSlice";
+const applicationSlice = createSlice({
+    name:'application',
+    initialState:{
+        applicants:[],
+    },
+    reducers:{
+        setAllApplicants:(state, action) => {
+            state.applicants = action.payload;
+        }
+    }
+});
+export const {setAllApplicants} = applicationSlice.actions;
+export default applicationSlice.reducer;
